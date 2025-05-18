f2a2589c6dc59e7165472107ef94752535994c53
d.mavlyutov <d.mavlyutov@v6services.ru>
2025-05-18 21:20:46 +0300

    Добавлено управление отступом кнопки воспроизведения

--- src/components/Player/PlayerControls.tsx
+++ src/components/Player/PlayerControls.tsx
@@ -7,11 +7,13 @@ interface PlayerControlsProps {
 }
 
 const PlayerControls: React.FC<PlayerControlsProps> = ({ onPlay, isPlaying }) => {
+  const plActive = !isPlaying ? 'pl-[0.188rem]' : 'pl-[0]';
+
   return (
     <div className="flex items-center gap-6">
       <button
         onClick={onPlay}
-        className="w-12 h-12 flex items-center justify-center rounded-full bg-white text-gray-900 shadow-lg hover:scale-105 pl-[0.188rem] transition cursor-[pointer]"
+        className={`w-12 h-12 flex items-center justify-center rounded-full bg-white text-gray-900 shadow-lg hover:scale-105 transition cursor-[pointer] ${plActive}`}
       >
         <span className="relative w-6 h-6">
           <Image
