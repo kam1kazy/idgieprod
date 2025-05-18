10aa412fdeec2c9e83bc62615c91fa99e18f25b1
d.mavlyutov <d.mavlyutov@v6services.ru>
2025-05-18 15:22:55 +0300

    Влючил turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    // Example: adding an alias and custom file extension
+    resolveAlias: {
+      underscore: 'lodash',
+      '@': './src',
+    },
+    resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.json'],
+  },
 };
 
 export default nextConfig;
