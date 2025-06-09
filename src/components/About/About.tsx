4efa9552809eb486fa0d0968f4df376d59b4aa3a
kam1kazy <t2nda@yandex.ru>
2025-06-09 15:08:54 +0300

    Добавлены новые изображения для компонента About, обновлен его контент и структура. Удалены устаревшие компоненты AboutContent и AboutTabs для упрощения кода.

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,11 +3,7 @@ import { useEffect, useRef } from 'react';
 
 import { updatePosition } from './hooks/updatePosition';
 
-const styles = {
-  textCenterAfter: `z-2 text-[4.875rem] w-auto absolute font-bold leading-[6.4625rem] after:content-[''] after:absolute after:top-auto after:bottom-[-1.875rem] after:-left-0 after:w-[4.063rem] after:h-[0.313rem] after:bg-[#87745f]`,
-};
-
-const AboutJournalWithImage: React.FC = () => {
+const About: React.FC = () => {
   const sectionRef = useRef<HTMLElement>(null);
   const titleRef = useRef<HTMLHeadingElement>(null);
 
@@ -19,10 +15,8 @@ const AboutJournalWithImage: React.FC = () => {
       animationFrameId = requestAnimationFrame(animate);
     };
 
-    // Запускаем анимацию
     animationFrameId = requestAnimationFrame(animate);
 
-    // Очистка
     return () => {
       if (animationFrameId) {
         cancelAnimationFrame(animationFrameId);
@@ -30,65 +24,68 @@ const AboutJournalWithImage: React.FC = () => {
     };
   }, []);
 
-  const textBlocks = [
-    {
-      title: 'Профессия',
-      content: 'Продюсер. Сонграйтер. Звукорежиссёр. Артист. Человек и пароход',
-    },
-    {
-      title: 'Опыт',
-      content: 'За спиной: 1000 аранжировок, 1000 сведённых треков, 300 авторских песен',
-    },
-    {
-      title: 'Жанры',
-      content: 'Поп, хип-хоп, R&B, поп-рок, хаус, бэйс',
-    },
-    {
-      title: 'Достижения',
-      content:
-        'Мои треки попадают в музыкальные чарты. На YouTube — отдельные релизы преодолели отметку в 20 миллионов. Помимо собственных проектов, я создавал саундтреки в сотрудничество с Wargaming.',
-    },
-    {
-      title: 'Философия',
-      content:
-        'Сегодня плагинов — море, но настоящий звук со вкусом — дефицит. Я не работаю по шаблону. Я создаю треки, которые хочется слушать снова и снова. Не ради 15 секунд в TikTok, а ради глубокого погружения — когда включаешь в наушниках и залипаешь надолго',
-    },
-  ];
-
   return (
     <section
       ref={sectionRef}
-      className="relative text-white py-16 bg-gradient-to-b from-black to-gray-900"
+      className="relative text-white pt-8 md:pt-40 pb-4 md:pb-20 bg-gradient-to-b from-black to-gray-900"
     >
-      <div className="w-[81.25rem] h-[60rem] mx-auto flex items-center justify-center">
-        <div className="relative flex flex-col md:flex-row items-center justify-between gap-[4.375rem] w-full">
-          <h2
-            ref={titleRef}
-            className={`${styles.textCenterAfter} will-change-transform text-shadow-md`}
-            style={{ transform: 'translateY(0)' }}
-          >
-            Дмитрий <br /> Эбергарт
-          </h2>
-          {/* Image */}
-          <div className="flex-1 relative h-[50rem]">
-            <Image
-              src="/images/idgie.png"
-              alt="Дмитрий Эбергарт"
-              fill
-              className="object-cover rounded-lg shadow-md"
-            />
+      {/* Gradient Overlay */}
+      <div className="w-full flex-col max-w-[81.25rem] px-4 mx-auto flex justify-center relative z-20">
+        <h2 className="text-white mb-[2.8rem] text-[3rem] sm:text-[4rem] md:text-[4.875rem] font-bold">
+          ABOUT
+        </h2>
+        <div className="relative flex flex-col lg:flex-row items-start justify-between gap-8 md:gap-[4.375rem] w-full">
+          {/* Image and Name/Quote */}
+          <div className="flex-1   flex-col items-center lg:items-start text-center lg:text-left">
+            <div className="relative w-full h-[20rem] sm:h-[30rem] md:h-[35rem] lg:h-[40rem] max-w-[25rem] lg:max-w-none">
+              <Image
+                src="/images/idgie-crop.jpg"
+                alt="Дмитрий Эбергарт"
+                fill
+                className="object-cover rounded-lg shadow-md"
+              />
+            </div>
+            <h3 className="text-white text-4xl font-bold mt-8">ДМИТРИЙ ЭБЕРГАРТ aka IDGIE</h3>
+            <p className="text-gray-300 text-3xl italic mt-4">
+              Музыка - мой дом. Заходи, пожалуйста. <br /> Я точно знаю, как в нём строить хиты!
+            </p>
           </div>
+
           {/* Text Content */}
-          <div className="flex-1 max-w-prose relative">
-            <h2 className="text-5xl font-bold mb-12 after:content-[''] after:absolute after:bottom-[-0.5rem] after:left-0 after:w-16 after:h-1 after:bg-[#87745f]">
-              About
-            </h2>
-            {textBlocks.map((block, index) => (
-              <div key={index} className="mb-6 pb-4 border-b border-gray-700 last:border-b-0">
-                <h3 className="text-xl font-semibold mb-2 text-[#87745f]">{block.title}</h3>
-                <p className="text-gray-300 text-[1.2rem] leading-relaxed">{block.content}</p>
+          <div className="flex-1 gap-[1.5rem] flex flex-col max-w-prose relative mt-8 lg:mt-0 lg:ml-auto">
+            <p className="text-gray-300 text-base md:text-[1.4rem] leading-relaxed mb-4">
+              30 лет. 14 лет в индустрии, 11 из них на проф. уровне
+            </p>
+            <p className="text-white text-xl md:text-2xl font-bold mb-8">
+              Продюсер. Сонграйтер. Звукорежиссёр. Артист. Человек и пароход.
+            </p>
+
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mb-8">
+              <div className="text-center">
+                <p className="text-orange-500 text-[3.6rem] font-bold">1000+</p>
+                <p className="text-gray-500 text-base">аранжировок</p>
               </div>
-            ))}
+              <div className="text-center">
+                <p className="text-orange-500 text-[3.6rem] font-bold">1000+</p>
+                <p className="text-gray-500 text-base">сведенных треков</p>
+              </div>
+              <div className="text-center">
+                <p className="text-orange-500 text-[3.6rem] font-bold">300+</p>
+                <p className="text-gray-500 text-base">авторских песен</p>
+              </div>
+            </div>
+
+            <p className="text-white text-lg md:text-xl font-semibold mb-2">Жанры:</p>
+            <p className="text-gray-300 text-base md:text-[1.4rem] leading-relaxed mb-4">
+              Поп, хип-хоп, рок, R&B, хаус, бэйс и многие другие <br />
+              Мои треки попадали в топ-чарты <br />
+              Некоторые — собирали 20+ млн на YouTube
+            </p>
+
+            <p className="text-gray-300 text-base md:text-[1.4rem] leading-relaxed mb-2">
+              Также писал саундтреки для игр компании Wargaming <br />И работал над саунд-дизайном
+              короткометражек
+            </p>
           </div>
         </div>
       </div>
@@ -96,4 +93,4 @@ const AboutJournalWithImage: React.FC = () => {
   );
 };
 
-export default AboutJournalWithImage;
+export default About;
