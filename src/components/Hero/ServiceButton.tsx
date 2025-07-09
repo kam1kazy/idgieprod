69d8d718c1772c8dbb98e92f56d5d9e460783a4a
kam1kazy <t2nda@yandex.ru>
2025-07-09 11:13:00 +0300

    Поправил мобильные стили

--- src/components/Hero/ServiceButton.tsx
+++ src/components/Hero/ServiceButton.tsx
@@ -12,12 +12,12 @@ const ServiceButton: React.FC<ServiceButtonProps> = ({ title, description, onCli
   return (
     <button
       onClick={onClick}
-      className={`w-full max-w-[340px] sm:w-auto px-4 py-3 md:px-6 md:py-4 group cursor-pointer transition-transform relative hover:scale-105 ${styles.textCenterAfter}`}
+      className={`w-full max-w-[340px] sm:w-auto mb-[10px] px-4 py-3 md:px-6 md:py-4 group cursor-pointer transition-transform relative hover:scale-105 ${styles.textCenterAfter}`}
     >
-      <div className="text-lg md:text-lg uppercase font-semibold mb-1 md:mb-2 group-hover:text-[#87745f] transition-colors">
+      <div className="text-md md:text-lg uppercase font-semibold mb-1 md:mb-2 group-hover:text-[#87745f] transition-colors">
         {title}
       </div>
-      <div className="text-md md:text-sm text-gray-300 h-auto md:h-[2.5rem]">{description}</div>
+      <div className="text-sm md:text-sm text-gray-300 h-auto md:h-[2.5rem]">{description}</div>
     </button>
   );
 };
